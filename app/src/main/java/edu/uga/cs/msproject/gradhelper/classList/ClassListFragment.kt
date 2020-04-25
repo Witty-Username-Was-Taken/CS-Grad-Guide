3482cb0af560b2763d29da7c98ee26ba5dc5692d
Tripp <tripp.guinn@outlook.com>
2020-04-25 16:52:28 -0400

    Search functionality added to Class List module

--- app/src/main/java/edu/uga/cs/msproject/gradhelper/classList/ClassListFragment.kt
+++ app/src/main/java/edu/uga/cs/msproject/gradhelper/classList/ClassListFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,11 +15,13 @@ import androidx.recyclerview.widget.RecyclerView
 import edu.uga.cs.msproject.gradhelper.R
 import edu.uga.cs.msproject.gradhelper.dataObjects.ClassItem
 import edu.uga.cs.msproject.gradhelper.dataObjects.ClassListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ClassListFragment : Fragment(),
 ClassListRecyclerViewAdapter.ClassSelectionRecyclerViewClickListener {
 
-    lateinit var classListRecyclerView: RecyclerView
+    lateinit var classListRecyclerView : RecyclerView
+    lateinit var classListSearchView : SearchView
     private lateinit var classListViewModel: ClassListViewModel
     private var listener : OnClassListInteractionListener? = null
 
@@ -43,12 +46,15 @@ ClassListRecyclerViewAdapter.ClassSelectionRecyclerViewClickListener {
         super.onActivityCreated(savedInstanceState)
 
         classListRecyclerView = view!!.findViewById(R.id.class_list_recycler_view)
+        classListSearchView = view!!.findViewById(R.id.class_list_search_view)
         val adapter = ClassListRecyclerViewAdapter(this)
 
         classListRecyclerView.adapter = adapter
         classListRecyclerView.layoutManager = LinearLayoutManager(activity)
 
         classListViewModel = ViewModelProvider(this).get(ClassListViewModel::class.java)
+        classListSearchView.setOnQueryTextListener(QueryTextListener(this, adapter))
+        classListSearchView.setOnCloseListener(SearchViewCloseListener(classListSearchView))
 
         classListViewModel.allClasses.observe(this.viewLifecycleOwner, Observer { classes ->
             classes?.let { adapter.setClasses(classes) }
@@ -73,6 +79,34 @@ ClassListRecyclerViewAdapter.ClassSelectionRecyclerViewClickListener {
 
     override fun classListItemClicked(classItem: ClassItem) {
         listener?.onClassListInteraction(classItem)
+    }
+
+    inner class QueryTextListener(fragment: Fragment, adapter: ClassListRecyclerViewAdapter) : SearchView.OnQueryTextListener {
+
+        val recyclerAdapter = adapter
+        val localFragment = fragment
+
+        override fun onQueryTextSubmit(p0: String?): Boolean {
+            return true
+        }
+
+        override fun onQueryTextChange(query: String?): Boolean {
+            classListViewModel.allClasses.observe(localFragment.viewLifecycleOwner, Observer { classes ->
+                classes?.let { recyclerAdapter.setClasses(classes.filter { it.course_name.contains(query!!) }) }
+            })
+
+            return true
+        }
+
+    }
+
+    inner class SearchViewCloseListener(searchView : SearchView) : SearchView.OnCloseListener {
+        val search = searchView
+
+        override fun onClose(): Boolean {
+            search.clearFocus()
+            return true
+        }
     }
 
 }
